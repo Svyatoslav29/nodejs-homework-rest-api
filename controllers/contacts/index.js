397f60d43f78d60454f1f3794411b1ecb8e50340
Svyatoslav29 <svyatoslav29@gmail.com>
2021-12-30 17:40:08 +0200

    fix: удалил и добавил  в гит игнор DS_Store

--- controllers/contacts/index.js
+++ controllers/contacts/index.js
@@ -3,7 +3,7 @@ import { HttpCode } from '../../lib/constants';
 
 const getContacts = async (req, res, next) => {
   const contacts = await repositoryContacts.listContacts(req.query)
-  res.status(HttpCode.OK).json({ status: 'success', code: HttpCode.OK, data: { ...contacts } })
+  res.status(HttpCode.OK).json({ status: 'success', code: HttpCode.OK, data: { contacts } })
 };
 
 const getContactById = async (req, res, next) => {
